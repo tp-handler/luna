6bdb08aabc45b70ab310d38294a3bb73d15fa834
tp-handler <kz5dpdbnyy@privaterelay.appleid.com>
2025-11-25 17:52:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[0;36m'
 NC='\033[0m'
-LUNA="https://raw.githubusercontent.com/tp-handler/luna/main/LunaOWO.zip"
+LUNA="https://raw.githubusercontent.com/tp-handler/luna/main/Luna.zip"
 
 APP_NAME="LunaMac"
 TEMP_DIR=$(mktemp -d)
